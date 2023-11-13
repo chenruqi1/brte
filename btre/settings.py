05f0fb139ff8a60aacdd730156e1bd0d9a3ed3c8
chenruqi <chenruqi@ksu.edu>
2023-11-13 10:45:31 -0600

    7th  time modificaion

--- btre/settings.py
+++ btre/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bredb',
+        'USER': 'postgres',
+        'PASSWORD':'0911',
+        'HOST': 'localhost',
     }
 }
+
 
 
 # Password validation
